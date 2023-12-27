fe18fc6d26b7d0bfafb28500d6687d0a21981710
Jonas <jonas.styger@gmail.com>
2023-12-27 14:23:31 +0100

    pause

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,10 @@
+
+// PascalCasing
+function Message(){
+    const name = 'Jonas';
+    if (name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello No Name</h1>
+}
+
+export default Message;
